c7a6cbcb6c45fa0b1379682cf2880182e5ccfe9c
mohammad6987 <zare.6984@gmail.com>
2025-09-13 16:15:42 +0330

    added model and repository codes for account service

--- transaction-service/src/main/java/com/example/transaction_service/repos/AccountRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.transaction_service.repos;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.transaction_service.models.Account;
-public interface AccountRepo extends JpaRepository<Account, String> {
-    List<Account> findByCustomerId(Long customerId);
-}
