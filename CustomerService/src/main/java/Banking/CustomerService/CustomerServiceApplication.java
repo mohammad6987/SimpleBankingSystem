b7236444679861a8addeabcffe39316cd0690a81
mohammad6987 <zare.6984@gmail.com>
2025-09-15 00:14:14 +0330

    added swagger dependencies and code

--- CustomerService/src/main/java/Banking/CustomerService/CustomerServiceApplication.java
+++ CustomerService/src/main/java/Banking/CustomerService/CustomerServiceApplication.java
@@ -3,6 +3,8 @@ package Banking.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableSwagger2
 @SpringBootApplication
 public class CustomerServiceApplication {
 
