8b7b41291008c957ae6b685e4d4746f99316a0db
mohammad6987 <zare.6984@gmail.com>
2025-09-14 23:46:13 +0330

    added basic authentication for both of services

---
+++ CustomerService/src/main/java/Banking/CustomerService/Config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package Banking.CustomerService.Config;
+
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        return http.build();
+    }
+
+    @Bean
+    public JdbcUserDetailsManager userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+            "SELECT username, password, enabled FROM users WHERE username = ?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+            "SELECT username, authority FROM authorities WHERE username = ?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
